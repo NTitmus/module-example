13cc819eaef2b339a6e93d10e7d1fc822a8edae9
Nicola Titmus <nicola.koo@cantab.net>
2023-12-08 21:54:12 +0000

    Initial commit - working book code + .gitignore

---
+++ prod/services/webserver-cluster/main.tf
@@ -0,0 +1,61 @@
+terraform {
+  required_version = ">= 1.0.0, < 2.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+
+
+  backend "s3" {
+    # This backend configuration is filled in automatically at test time by Terratest. If you wish to run this example
+    # manually, uncomment and fill in the config below.
+
+    bucket         = "terraform-book-state-nt2"
+    key            = "prod/services/webserver-cluster/terraform.tfstate"
+    region         = "eu-west-2"
+    dynamodb_table = "terraform-book-locks2"
+    encrypt        = true
+  }
+}
+
+provider "aws" {
+  region = "eu-west-2"
+}
+
+module "webserver_cluster" {
+  source = "../../../modules/services/webserver-cluster"
+
+  cluster_name           = var.cluster_name
+  db_remote_state_bucket = var.db_remote_state_bucket
+  db_remote_state_key    = var.db_remote_state_key
+
+  instance_type = "m4.large"
+  #"m4.large"
+  #t2.micro
+  min_size      = 2
+  max_size      = 10
+}
+
+resource "aws_autoscaling_schedule" "scale_out_during_business_hours" {
+  scheduled_action_name = "scale-out-during-business-hours"
+  min_size              = 2
+  max_size              = 10
+  desired_capacity      = 10
+  recurrence            = "0 9 * * *"
+
+  autoscaling_group_name = module.webserver_cluster.asg_name
+}
+
+resource "aws_autoscaling_schedule" "scale_in_at_night" {
+  scheduled_action_name = "scale-in-at-night"
+  min_size              = 2
+  max_size              = 10
+  desired_capacity      = 2
+  recurrence            = "0 17 * * *"
+
+  autoscaling_group_name = module.webserver_cluster.asg_name
+}
+
